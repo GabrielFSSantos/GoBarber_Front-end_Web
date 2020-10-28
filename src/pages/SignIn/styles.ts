2624baf60024d7f79876abde5c226ad9191029ca
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2020-10-28 01:13:30 -0300

    Login page

---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,103 @@
+import styled from 'styled-components';
+import {shade} from 'polished';
+
+import signInbBackgroundImg from '../../assets/sign-in-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    input {
+      background: #232129;
+      border-radius: 10px;
+      border: 2px solid #232129;
+      padding: 16px;
+      width: 100%;
+      color: #F4EDE8;
+
+      &::placeholder {
+        color: #666360;
+      }
+
+      & + input {
+        margin-top: 8px;
+      }
+    }
+
+    button {
+      background: #FF9000;
+      height: 56px;
+      width: 100%;
+      margin-top: 16px; 
+      padding: 0 16px;
+      border-radius: 10px;
+      border: 0;
+      color: #312E38;
+      font-weight: 500;
+      transition: background-color 0.2s;
+
+      &:hover {
+        background: ${shade(0.2, '#FF9000')}
+      }
+    }
+
+    a {
+      color: #F4EDE8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#F4EDE8')}
+      }
+    }
+
+  }
+
+  > a {
+    color: #FF9000;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    display: flex;
+    align-items: center;
+
+    &:hover {
+      color: ${shade(0.2, '#FF9000')}
+    }
+
+    svg {
+      margin-right: 16px
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signInbBackgroundImg}) no-repeat center;
+  background-size: cover;
+`;
