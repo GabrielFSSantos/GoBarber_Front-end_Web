015d0735f8495ab0d018131920421657722706fd
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2020-10-29 14:01:23 -0300

    Validating registration

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {FiArrowLeft, FiMail, FiUser, FiLock} from 'react-icons/fi';
 import {Form} from '@unform/web';
+import * as Yup from 'yup';
 
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
@@ -10,9 +11,22 @@ import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
 
-  function handleSubmit(data: object) {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        nome: Yup.string().required('Nome Obrigatório'),
+        email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No minímo 6 digitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
   
   return (
     <Container>
